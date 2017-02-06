1e700aed3ed55d9499f4fea1389d54f40fb7c364
1013716411 <asD8360586>
2017-02-06 23:39:32 +0800

    message

--- js/mz.js
+++ js/mz.js
@@ -4,40 +4,50 @@
             $(".nav li").hover(function(){
 
                 $(".bg").stop().animate({height:74+$(this).children(".two").height()},400)
-                $(this).children(".two").stop(false,true).slideDown(300)
+                $(this).children(".two").stop(false,true).slideDown(500)
             },function(){
                 $(".bg").stop().animate({height:74},300)
-                $(this).children(".two").stop(false,true).slideUp(300)
+                $(this).children(".two").stop(false,true).slideUp(600)
                 
             })
+                //第二菜单
+            $(".btn").click(function(){
+               $(this).find("dl").stop(false,true).slideToggle(600);
+         })
+           
+     })
+      //联系按钮
+       $('.tail,.tail-dd').click(function(){
+                  $('html,body').animate({scrollTop:$('.footer').offset().top},1000);
+              })
 
-        //魅族
-            var win_h =$(window).height()*0.7;
-            var con1_t = $(".con1 .inner").offset().top-win_h;
-            var con2_t = $(".con2 .inner").offset().top-win_h;
-            var con3_t = $(".con3 .inner").offset().top-win_h;
-            var con4_t = $(".con4 .inner").offset().top-win_h;
+        //文字
+            var Herght_h =$(window).height()*0.7;
+            var con1_t = $(".con1 .inner").offset().top-Herght_h;
+            var con2_t = $(".con2 .inner").offset().top-Herght_h;
+            var con3_t = $(".con3 .inner").offset().top-Herght_h;
+            var con4_t = $(".con4 .inner").offset().top-Herght_h;
             $(window).scroll(function(){
-                var s_top = $(this).scrollTop();
-                if(s_top >= con1_t){
+                var top_h = $(this).scrollTop();
+                if(top_h >= con1_t){
                         $(".con1 .inner").addClass("act")
                 }
-                if(s_top >= con2_t){
+                if(top_h >= con2_t){
                         $(".con2 .inner").addClass("act")
                 }
-                if(s_top >= con3_t){
+                if(top_h >= con3_t){
                         $(".con3 .inner").addClass("act")
                 }
-                if(s_top >= con4_t){
+                if(top_h >= con4_t){
                         $(".con4 .inner").addClass("act")
                 }
             })
           //返回顶部
             $(window).scroll(function(){
                 if($(window).scrollTop()>100){
-                    $("#top").fadeIn(1500)
+                    $("#top").fadeIn(1000)
                 }else{
-                    $("#top").fadeOut(1500)
+                    $("#top").fadeOut(600)
                 }
             })
             $("#top").click(function(){
@@ -47,10 +57,6 @@
                    $("body").animate({scrollTop:0},1000)
                 }
             })
-            $(".btn").click(function(){
-               $(this).find("dl").stop(false,true).slideToggle(600);
-         })
-           
-     })
+
        
        
